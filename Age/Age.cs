e6b00d9b2df0e2a1bdb3c2058f648d5457aacfc0
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-10-13 22:08:10 +0300

    RedoAge

--- Age/Age.cs
+++ Age/Age.cs
@@ -3,14 +3,47 @@
  *  */
 
 using System;
+using System.Globalization;
 
 class Age
 {
     static void Main()
     {
-        DateTime inputDate = DateTime.Parse(Console.ReadLine());
-        int result = DateTime.Now.Year - inputDate.Year;
-        Console.WriteLine("{0}", result);
-        Console.WriteLine("{0}", result + 10);
+        CultureInfo provider = CultureInfo.InvariantCulture;
+        string formatDate = "MM.dd.yyyy";
+        string input = Console.ReadLine();
+        DateTime inputDate = DateTime.ParseExact(input, formatDate, provider);
+        // DateTime inputDate = DateTime.Parse(Console.ReadLine());
+        DateTime correctedNowDate = DateTime.Now.AddMonths(-7);
+        int result = 0;
+        if (correctedNowDate.Month > inputDate.Month)
+        {
+            //result = correctedNowDate.Year - inputDate.Year;
+            if (correctedNowDate.Date > inputDate.Date)
+            {
+                result = correctedNowDate.Year - inputDate.Year;
+            }
+            else if (correctedNowDate.Date <= inputDate.Date)
+            {
+                result = correctedNowDate.Year - inputDate.Year - 1;
+            }
+        }
+        else if (correctedNowDate.Month == inputDate.Month)
+        {
+            if (correctedNowDate.Date > inputDate.Date)
+            {
+                result = correctedNowDate.Year - inputDate.Year;
+            }
+            else if (correctedNowDate.Date <= inputDate.Date)
+            {
+                result = correctedNowDate.Year - inputDate.Year - 1;
+            }
+        }
+        else
+        {
+            result = correctedNowDate.Year - inputDate.Year - 1;
+        }
+        Console.WriteLine(result);
+        Console.WriteLine(result + 10);
     }
 }
